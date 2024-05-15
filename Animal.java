7d46a826accb0a43fb381ccf9cd892f2d25460b4
ceciliaeskinazi <168425472+ceciliaeskinazi@users.noreply.github.com>
2024-05-15 10:43:44 -0300

    Create Animal.java

---
+++ Animal.java
@@ -0,0 +1,34 @@
+package aulaExtra;
+
+public class Animal {
+	private String nome;
+	private String tipo;
+	
+	
+	public Animal(String novoNome, String novoTipo) {
+		this.nome = novoNome;
+		this.tipo = novoTipo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	
+	public String getTipo() {
+		return tipo;
+	}
+	
+	
+	public void setNome(String novoNome) {
+		this.nome = novoNome;
+	}
+	
+	public void setTipo(String novoTipo) {
+		this.tipo = novoTipo;
+		
+	}
+	
+	public String emitirSom(String a) {
+		return a;
+	}
+
+}
