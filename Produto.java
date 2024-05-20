d947fe6c1456d2deb9d85351a533607842ee913f
ceciliaeskinazi <168425472+ceciliaeskinazi@users.noreply.github.com>
2024-05-20 08:50:29 -0300

    Create Produto.java

---
+++ Produto.java
@@ -0,0 +1,37 @@
+
+public class Produto {
+	private String nome;
+	private double preco;
+	private int quantidade;
+	
+	public Produto (String novoNome, double novoPreco, int novoQuantidade); {
+		this.nome = novoNome;
+		this.preco = novoPreco;
+		this.quantidade = novoQuantidade;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public double getPreco() {
+		return preco;
+	}
+	public int getQuantidade() {
+		return quantidade;
+	}
+	
+	public void setNome(String novoNome) {
+		this.nome = novoNome;
+	}
+	public void setPreco(double novoPreco) {
+		this.preco = novoPreco;
+	}
+	public void setQuantidade(int novoQuantidade) {
+		this.quantidade = novoQuantidade;
+	}
+	
+	public double calcularTotal() {
+		return preco * quantidade;
+	}
+	
+}
