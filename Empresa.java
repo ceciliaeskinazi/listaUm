a5301831687af400fc8cbe6ced26120a40df8227
ceciliaeskinazi <168425472+ceciliaeskinazi@users.noreply.github.com>
2024-05-20 10:46:48 -0300

    Create Empresa.java

---
+++ Empresa.java
@@ -0,0 +1,24 @@
+public class Empresa {
+	private String nome;
+	private String cnpj;
+	
+	public Empresa(String novoNome, String novoCnpj) {
+		this.nome = novoNome;
+		this.cnpj = novoCnpj;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setNome(String novoNome) {
+		this.nome = novoNome;
+	}
+	public void setCnpj(String novoCnpj) {
+		this.cnpj = novoCnpj;
+	}
+
+}
